7f16af1dfb6d17b474798afd58777295a7384714
helenemolberg <helenemolberg@gmail.com>
2020-05-12 16:44:11 +0200

    done with method exactMPS

--- src/main/java/G35HW2.java
+++ src/main/java/G35HW2.java
@@ -1,3 +1,4 @@
+import breeze.linalg.VectorConstructors;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.mllib.linalg.Vector;
@@ -7,6 +8,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class G35HW2 {
 
@@ -29,7 +31,7 @@ public static void main(String[] args) throws IOException {
     JavaSparkContext sc = new JavaSparkContext(conf);
     sc.setLogLevel("WARN");
 
-    System.out.println(inputPoints);
+    //System.out.println(inputPoints);
 
     G35HW2.exactMPS(inputPoints);
 
@@ -41,6 +43,7 @@ public static void main(String[] args) throws IOException {
 // Auxiliary methods
 // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
+    //Is running through readVectorsSeq - making a list of [double, double]
     public static Vector strToVector(String str) {
         String[] tokens = str.split(",");
         double[] data = new double[tokens.length];
@@ -65,9 +68,37 @@ public static void main(String[] args) throws IOException {
 
     //receives in input a set of points S and returns the max distance between two points in S
     private static void exactMPS(ArrayList<Vector> inputPoints) {
+        //Starting time
+        long startTime = System.currentTimeMillis();
+
+        //Setting variables
+        double distance = 0;
+        double cal;
+
+        //Uses this to make a sublist of inputPoints
+        int size = inputPoints.size();
+
+        //System.out.println(inputPoints);
+        //System.out.println(size);
+
+        //Uses two for-loops to iterate through two vectors
+        //The first vector with all the elements from inputPoints
+        //The second vector with all the elements from place 1
+        for(Vector vector : inputPoints){
+            for(Vector vector2 : inputPoints.subList(1, size)){
+                //Uses the formula for calculating distance. - sqrt((x1-x0)^2 + (y1-y0)^2)
+                cal = Math.sqrt(((vector2.apply(0)-vector.apply(0))*(vector2.apply(0)-vector.apply(0))) +
+                        ((vector2.apply(1)-vector.apply(1))*(vector2.apply(1)-vector.apply(1))));
+                if (cal >= distance) distance = cal;
+
+            }
+        }
+
+        //Ending time
+        long endTime = System.currentTimeMillis();
 
         System.out.println("EXACT ALGORITHM");
-        System.out.println("Max distance = ");
-        System.out.println("Running time = ");
+        System.out.println("Max distance = " + distance);
+        System.out.println("Running time = " + (endTime - startTime) + " milliseconds");
     }
 }
